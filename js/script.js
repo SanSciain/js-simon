9969b333fa08c0fbff711a9d709ecfc069f1ae03
SanSciain <mastroeni.santi@gmail.com>
2022-04-27 14:04:36 +0200

    Add: all html, all js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,40 @@
 
 const printNumber = document.getElementById("numbers");
 
-let numbersArray;
+const numbersArray=[];
+let i = 0;
+
+while (i<5){
+    number = getRndInteger(1,99);
+    if(!(numbersArray.includes(number))){
+        numbersArray.push(number);
+        i++;
+    }
+}
+
+printNumber.innerHTML= numbersArray;
+
+setTimeout(gameStart,3000);
 
 
+function gameStart(){
+    printNumber.innerHTML="";
+    setTimeout(guessNumbers,1);
+}
+
+
+
+function guessNumbers(){
+    
+    const guessedNumbers = [];
+    for (let j=0; j<5;j++){
+        guessNumber=parseInt(prompt("Che numero ricordi?"));
+        if (numbersArray.includes(guessNumber)){
+            guessedNumbers.push(guessNumber);
+        }
+    }
+    printNumber.innerHTML= `hai indovinato ${guessedNumbers.length} numeri : ${guessedNumbers}`;
+}
 
 
 function getRndInteger(min, max) {
